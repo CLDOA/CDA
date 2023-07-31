fc8aa188afa96368785b70a8ff89109a767b75b4
CLDOA <136110214+CLDOA@users.noreply.github.com>
2023-07-31 17:13:08 +0530

    Update 31/07/2023

--- login/assets/loader.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    setTimeout(function () {
-      document.getElementById('loader-conatiner').style.display = 'none';
-      document.getElementById('main-content').style.display = 'block';
-    }, 2500);
-  });
